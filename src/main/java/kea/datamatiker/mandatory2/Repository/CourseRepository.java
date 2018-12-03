f615e14bef573842716710ee7f8d9bafe19e6163
muddihejjo <33859403+muddihejjo@users.noreply.github.com>
2018-12-03 08:51:14 +0100

    Repositories

---
+++ src/main/java/kea/datamatiker/mandatory2/Repository/CourseRepository.java
@@ -0,0 +1,16 @@
+package kea.datamatiker.mandatory2.Repository;
+
+import kea.datamatiker.mandatory2.Model.Course;
+import org.springframework.data.repository.CrudRepository;
+
+import java.util.List;
+
+
+public interface CourseRepository extends CrudRepository<Course, Long>{
+        Course findCourse(Long id);
+        List<Course> findAll();
+
+        }
+
+
+
