42018dfa734d4cfb0be1e2e4afac0b44eeee55e2
Anique Uddin <Anique_uddin@live.dk>
2018-12-05 17:10:36 +0100

    slutte af CRUD til alle funktionerne. Fokus på gemme til databsen

--- src/main/java/kea/datamatiker/mandatory2/Controller/TeacherController.java
+++ src/main/java/kea/datamatiker/mandatory2/Controller/TeacherController.java
@@ -1,6 +1,7 @@
 package kea.datamatiker.mandatory2.Controller;
 
 import kea.datamatiker.mandatory2.Model.Course;
+import kea.datamatiker.mandatory2.Model.Teacher;
 import kea.datamatiker.mandatory2.Repository.CourseRepository;
 import kea.datamatiker.mandatory2.Repository.TeacherRepository;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -19,9 +20,17 @@ public class TeacherController {
     private TeacherRepository teacherRepository;
 
     @GetMapping("/teacher/courses")
-    public String teacher(Model m){
+    public String showCourses(Model m){
         List<Course> courses = courseRepository.findAll();
         m.addAttribute("coursesToBeSendToView", courses);
         return "teacher/index";
+    }
+
+    @GetMapping("/course/create")
+    public String addCourses(Model m){
+        m.addAttribute("course", new Course());
+        Iterable<Teacher> teachers = teacherRepository.findAll();
+        m.addAttribute("Teachers", teachers);
+        return "teacher/courseNew";
     }
 }
